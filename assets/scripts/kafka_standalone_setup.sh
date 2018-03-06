e1bb3b5125430ff0260d7737898c6d84d1b5c128
Victor <>
2018-03-06 15:50:56 +0100

    Add start command

--- assets/scripts/kafka_standalone_setup.sh
+++ assets/scripts/kafka_standalone_setup.sh
@@ -3,9 +3,14 @@ apt-get update && \
     apt-get install wget -y && \
     rm -rf /var/lib/apt/lists/* && \
     apt-get clean && \
-    apt-get install openjdk-8-jre -y && \
     apt-get install vim -y && \
-    echo java -version
+
+#install java
+apt-get update
+apt-get install openjdk-8-jre
+echo java -version
+
+#note that default broker.id = 0
 
 export ZOOKEEPER_VERSION=3.4.11
 export SCALA_VERSION=2.12
@@ -13,9 +18,8 @@ export KAFKA_VERSION=1.0.0
 export KAFKA_HOME=/opt/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION"
 export KAFKA_DOWNLOAD_URL=https://archive.apache.org/dist/kafka/"$KAFKA_VERSION"/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION".tgz
 
-export ADVERTISED_PORT=
-
 wget -q $KAFKA_DOWNLOAD_URL -O /tmp/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION".tgz
 tar xfz /tmp/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION".tgz -C /opt && rm /tmp/kafka_"$SCALA_VERSION"-"$KAFKA_VERSION".tgz
 
-start-kafka.sh
+# Run Kafka
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties

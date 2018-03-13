9172ac0a99060c659f850f71358ceacaac47fa91
Victor <>
2018-03-13 09:09:32 +0100

    Add Zookeeper standalone script

---
+++ assets/scripts/zookeeper_standalone_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+export ZOOKEEPER_VERSION=3.4.11
+export ZOOKEEPER_HOME=/opt/zookeeper-"$ZOOKEEPER_VERSION"
+
+wget -q http://ftp.cixug.es/apache/zookeeper/zookeeper-"$ZOOKEEPER_VERSION"/zookeeper-"$ZOOKEEPER_VERSION".tar.gz -O /tmp/zookeeper-"$ZOOKEEPER_VERSION".tgz
+ls -l /tmp/zookeeper-"$ZOOKEEPER_VERSION".tgz
+tar xfz /tmp/zookeeper-"$ZOOKEEPER_VERSION".tgz -C /opt && rm /tmp/zookeeper-"$ZOOKEEPER_VERSION".tgz
+
+$ZOOKEEPER_HOME/bin/zkServer.sh start-foreground
